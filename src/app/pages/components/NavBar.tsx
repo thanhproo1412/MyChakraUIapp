05576f2e182b55d3eb2696556cbd96415317ac2e
NGUYỄN SỸ HẢI THANH <thanhproo1412@gmail.com>
2023-11-30 23:37:08 +0700

    update nav

--- src/app/pages/components/NavBar.tsx
+++ src/app/pages/components/NavBar.tsx
@@ -1,71 +1,305 @@
 'use client'
 
-import Image from 'next/image'
-import Link from "next/link";
-import styles from "./css/NavBarCss.module.css";
-import { useState } from 'react';
+import {
+    Box,
+    Flex,
+    Text,
+    IconButton,
+    Button,
+    Stack,
+    Collapse,
+    Icon,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    useColorModeValue,
+    useBreakpointValue,
+    useDisclosure,
+    Image
+} from '@chakra-ui/react'
+import {
+    HamburgerIcon,
+    CloseIcon,
+    ChevronDownIcon,
+    ChevronRightIcon,
+} from '@chakra-ui/icons'
 
-export default function NavBar() {
-
-    const [isOpen, setIsOpen] = useState(false)
+export default function WithSubnavigation() {
+    const { isOpen, onToggle } = useDisclosure()
 
     return (
+        <Box>
+            <Flex
+                bg={useColorModeValue('white', 'gray.800')}
+                color={useColorModeValue('gray.600', 'white')}
+                minH={'60px'}
+                py={{ base: 2 }}
+                px={{ base: 4 }}
+                borderBottom={1}
+                borderStyle={'solid'}
+                borderColor={useColorModeValue('gray.200', 'gray.900')}
+                align={'center'}>
+                <Flex
+                    flex={{ base: 1, md: 'auto' }}
+                    ml={{ base: -2 }}
+                    display={{ base: 'flex', md: 'none' }}
+                >
+                    <IconButton
+                        onClick={onToggle}
+                        icon={isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />}
+                        variant={'ghost'}
+                        aria-label={'Toggle Navigation'}
+                    />
+                </Flex>
+                <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+                    <Text
+                        textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+                        fontFamily={'heading'}
+                        color={useColorModeValue('gray.800', 'white')}>
+                        <Image
+                            boxSize='50px'
+                            objectFit='cover'
+                            src='https://bit.ly/dan-abramov'
+                            alt='Dan Abramov'
+                        />
 
-        <nav className={`${styles['navbar']} `}>
-            <div className="container mx-auto flex justify-between items-center">
+                    </Text>
 
-                <ul className="flex">
-                    <Link href=""
-                        className={styles['logo']}><Image src='/HTlogo.png' alt="me" width="150" height="40" />
-                    </Link>
-                    <Link href="about" className={` ${styles['active']} `}><span>Trang chủ</span></Link>
+                    <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+                        <DesktopNav />
+                    </Flex>
+                </Flex>
 
-                    {/* dropdown */}
-                    <div>
-                        <button onClick={() => setIsOpen((prev) => !prev)} className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Tin tức <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-                        </svg></button>
-                        {isOpen && (
-                            <div className={`bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 ${styles['dropdown-menu']}`}>
-                                <ul className="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-                                    <li>
-                                        <a href="tinvinfast" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Tin VinFast</a>
-                                    </li>
-                                    <li>
-                                        <a href="tincongdong" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Tin Cộng Đồng</a>
-                                    </li>
-                                    <li>
-                                        <a href="tindoisong" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Đời Sống</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        )}
-                    </div>
+                <Stack
+                    flex={{ base: 1, md: 0 }}
+                    justify={'flex-end'}
+                    direction={'row'}
+                    spacing={6}>
+                    <Button as={'a'} fontSize={'sm'} fontWeight={400} variant={'link'} href={'#'}>
+                        Sign In
+                    </Button>
+                    <Button
+                        as={'a'}
+                        display={{ base: 'none', md: 'inline-flex' }}
+                        fontSize={'sm'}
+                        fontWeight={600}
+                        color={'white'}
+                        bg={'pink.400'}
+                        href={'#'}
+                        _hover={{
+                            bg: 'pink.300',
+                        }}>
+                        Sign Up
+                    </Button>
+                </Stack>
+            </Flex>
 
-                    <Link href="contact" className=""><span>Tin tức</span></Link>
-                    <Link href="login" className=""><span>Diễn đàn</span></Link>
-                    <Link href="signup" className=""><span>Sự kiện</span></Link>
-                    <Link href="signup" className=""><span>Thư viện</span></Link>
-                </ul>
-
-                <ul className="flex justify-content: center items-center">
-
-                    {/* search bar */}
-                    <div className="flex items-center pr-4">
-                        <form action="" className="relative mx-auto w-max flex">
-                            <input type="search" className={`peer cursor-pointer relative z-10 h-12 w-12 rounded-full border bg-transparent pl-12 outline-none ${styles['search-bar']} `} />
-                            <svg xmlns="http://www.w3.org/2000/svg" className="  absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-gray-500 px-3.5 peer-focus:border-lime-300 peer-focus:stroke-lime-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                            </svg>
-                        </form>
-                    </div>
-                    <div className={styles['search-separator']}></div>
-                    <Link href="login" className={`p-2 ${styles['login-button']} `}><span>Đăng nhập</span></Link>
-                    <Link href="signup" className={`p-2 ${styles['register-button']} `}><span>Đăng kí</span></Link>
-                </ul>
-
-            </div>
-        </nav >
-
+            <Collapse in={isOpen} animateOpacity>
+                <MobileNav />
+            </Collapse>
+        </Box>
     )
 }
+
+const DesktopNav = () => {
+    const linkColor = useColorModeValue('gray.600', 'gray.200')
+    const linkHoverColor = useColorModeValue('gray.800', 'white')
+    const popoverContentBgColor = useColorModeValue('white', 'gray.800')
+
+    return (
+        <Stack direction={'row'} spacing={4} justify={'center'} align={'center'}>
+            {NAV_ITEMS.map((navItem) => (
+                <Box key={navItem.label}>
+                    <Popover trigger={'hover'} placement={'bottom-start'}>
+                        <PopoverTrigger>
+                            <Box
+                                as="a"
+                                p={2}
+                                href={navItem.href ?? '#'}
+                                fontSize={'sm'}
+                                fontWeight={500}
+                                color={'#E53E3E'}
+                                _hover={{
+                                    textDecoration: 'none',
+                                    color: '#9DECF9',
+                                }}>
+                                {navItem.label}
+                            </Box>
+                        </PopoverTrigger>
+                        {navItem.children && (
+                            <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
+                        )}
+
+                        {navItem.children && (
+                            <PopoverContent
+                                border={0}
+                                boxShadow={'xl'}
+                                bg={popoverContentBgColor}
+                                p={4}
+                                rounded={'xl'}
+                                minW={'sm'}>
+                                <Stack>
+                                    {navItem.children.map((child) => (
+                                        <DesktopSubNav key={child.label} {...child} />
+                                    ))}
+                                </Stack>
+                            </PopoverContent>
+                            
+                        )}
+                    </Popover>
+                </Box>
+            ))}
+        </Stack>
+    )
+}
+
+const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
+    return (
+        <Box
+            as="a"
+            href={href}
+            role={'group'}
+            display={'block'}
+            p={2}
+            rounded={'md'}
+            _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}>
+            <Stack direction={'row'} align={'center'}>
+                <Box>
+                    <Text
+                        transition={'all .3s ease'}
+                        _groupHover={{ color: 'pink.400' }}
+                        fontWeight={500}>
+                        {label}
+                    </Text>
+                    <Text fontSize={'sm'}>{subLabel}</Text>
+                </Box>
+                <Flex
+                    transition={'all .3s ease'}
+                    transform={'translateX(-10px)'}
+                    opacity={0}
+                    _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+                    justify={'flex-end'}
+                    align={'center'}
+                    flex={1}>
+                    <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
+                </Flex>
+            </Stack>
+        </Box>
+    )
+}
+
+const MobileNav = () => {
+    return (
+        <Stack bg={useColorModeValue('white', 'gray.800')} p={4} display={{ md: 'none' }}>
+            {NAV_ITEMS.map((navItem) => (
+                <MobileNavItem key={navItem.label} {...navItem} />
+            ))}
+        </Stack>
+    )
+}
+
+const MobileNavItem = ({ label, children, href }: NavItem) => {
+    const { isOpen, onToggle } = useDisclosure()
+
+    return (
+        <Stack spacing={4} onClick={children && onToggle}>
+            <Box
+                py={2}
+                as="a"
+                href={href ?? '#'}
+                justifyContent="space-between"
+                alignItems="center"
+                _hover={{
+                    textDecoration: 'none',
+                }}>
+                <Text fontWeight={600} color={useColorModeValue('gray.600', 'gray.200')}>
+                    {label}
+                </Text>
+                {children && (
+                    <Icon
+                        as={ChevronDownIcon}
+                        transition={'all .25s ease-in-out'}
+                        transform={isOpen ? 'rotate(180deg)' : ''}
+                        w={6}
+                        h={6}
+                    />
+                )}
+            </Box>
+
+            <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
+                <Stack
+                    mt={2}
+                    pl={4}
+                    borderLeft={1}
+                    borderStyle={'solid'}
+                    borderColor={useColorModeValue('gray.200', 'gray.700')}
+                    align={'start'}>
+                    {children &&
+                        children.map((child) => (
+                            <Box as="a" key={child.label} py={2} href={child.href}>
+                                {child.label}
+                            </Box>
+                        ))}
+                </Stack>
+            </Collapse>
+        </Stack>
+    )
+}
+
+interface NavItem {
+    label: string
+    subLabel?: string
+    children?: Array<NavItem>
+    href?: string
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+    {
+        label: 'Trang Chủ',
+        subLabel: 'Up-and-coming Designers',
+    },
+    {
+        label: 'Tin tức',
+        children: [
+            {
+                label: 'Tin Vinfast',
+                subLabel: 'test subLabel',
+                href: '#',
+            },
+            {
+                label: 'Tin Cộng Đồng',
+                href: '#',
+            },
+            {
+                label: 'Đời Sống',
+                href: '#',
+                children: [
+                    {
+                        label: 'Thị trường xe điện',
+                        href: '#',
+                    },
+                    {
+                        label: 'Công nghệ',
+                        href: '#',
+                    },
+                    {
+                        label: 'Du lịch',
+                        href: '#',
+                    },
+                    {
+                        label: 'Ảnh đẹp',
+                        href: '#',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        label: 'Learn Design',
+        href: '#',
+    },
+    {
+        label: 'Hire Designers',
+        href: '#',
+    },
+]
